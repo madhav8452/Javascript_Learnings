53ded2268de8adb7c0971a3c6c07db507609a541
madhav8452 <madhavrathod019@gmail.com>
2024-09-03 15:27:55 +0530

    Day_10

---
+++ Day 10/double_square.js
@@ -0,0 +1,18 @@
+function double(num)
+{
+    let double = num * 2;
+    return double;
+}
+function square(num)
+{
+    return num 
+}
+function doublesquare(num)
+{
+
+}
+double();
+
+square();
+
+let result = doublesquare(3);
